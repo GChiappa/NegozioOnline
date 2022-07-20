ca972f86a46e255ea18957234f7cb908cbf638f7
Betacom <Betacom@DESKTOP-30EVD2S.fritz.box>
2022-07-20 17:25:44 +0200

    index e codici sql -GC

---
+++ NegozioOnline/src/main/webapp/resources/sql/insert_script.sql
@@ -0,0 +1,17 @@
+--articolo
+insert into articolo values(articolo_seq.nextval, 'Apple', 'Mac Pro', 2340);
+insert into articolo values(articolo_seq.nextval, 'Apple', 'IPhone 13', 1460);
+insert into articolo values(articolo_seq.nextval, 'Samsung', 'Galaxy Fold', 2170);
+insert into articolo values(articolo_seq.nextval, 'Samsung', 'Lavastoviglie XTR', 350);
+insert into articolo values(articolo_seq.nextval, 'HP', 'Laptop', 899);
+
+insert into immagine values(1, 'img/MacPro.jpg', 'Mac Pro');
+insert into immagine values(2, 'img/IPhone13.jpg', 'IPhone 13');
+insert into immagine values(3, 'img/GalaxyFold.jpg', 'Galaxy Fold');
+insert into immagine values(4, 'img/LavastoviglieXTR.jpg', 'Lavastoviglie XTR');
+insert into immagine values(5, 'img/LaptopHP.jpg', 'Laptop HP');
+
+insert into amministratore values('admin','e4k^Rd%fgT9t*t2TA£S5y!s58u_dak^Rd%fgT9t*t2TA£S5y!s58u_d3k^Rd%fgT9t*t2TA£S5y!s58u_29k^Rd%fgT9t*t2TA£S5y!s58u_bk^Rd%fgT9t*t2TA£S5y!s58u_dek^Rd%fgT9t*t2TA£S5y!s58u_f2k^Rd%fgT9t*t2TA£S5y!s58u_bbk^Rd%fgT9t*t2TA£S5y!s58u_e2k^Rd%fgT9t*t2TA£S5y!s58u_1ek^Rd%fgT9t*t2TA£S5y!s58u_20k^Rd%fgT9t*t2TA£S5y!s58u_adk^Rd%fgT9t*t2TA£S5y!s58u_49k^Rd%fgT9t*t2TA£S5y!s58u_48k^Rd%fgT9t*t2TA£S5y!s58u_dek^Rd%fgT9t*t2TA£S5y!s58u_90k^Rd%fgT9t*t2TA£S5y!s58u_','giovanni@admin.it');
+
+commit
+
